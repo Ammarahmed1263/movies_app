dc5e2e764b49397f415ef37b9c9313c12c59e4cc
Ammar Ahmed <ammarahmed1263@gmail.com>
2024-02-26 13:45:50 +0200

    simplify login and signup logic

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,23 +1,8 @@
-import {Button, ImageBackground, Text, TextInput, View} from 'react-native';
+import AuthContent from '../components/auth/AuthContent';
 
 function LoginScreen() {
-  return (
-    <View
-      style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}
-    >
-      <View
-        style={{
-          backgroundColor: 'rgba(255,255,255,0.7)',
-          width: '80%',
-          height: '40%',
-        }}>
-        <Text>Welcome back,</Text>
-        <TextInput>hello</TextInput>
-        <TextInput>hello</TextInput>
-        <Button title="click me" />
-      </View>
-    </View>
-  );
+  return <AuthContent isLogin />;
 }
 
 export default LoginScreen;
+
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,5 +1,5 @@
 function SignupScreen() {
-
+  return <AuthContent isLogin />;
 }
 
 export default SignupScreen;

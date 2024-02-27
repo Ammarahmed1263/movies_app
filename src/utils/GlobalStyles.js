0605432e9667893ffb8fa104a2cbdca11e2c95bc
Ammar Ahmed <ammarahmed1263@gmail.com>
2024-02-27 18:15:52 +0200

    change to lighter primary colors

--- src/utils/GlobalStyles.js
+++ src/utils/GlobalStyles.js
@@ -1,12 +1,10 @@
 import { StyleSheet } from "react-native";
 
 const GlobalStyles = StyleSheet.create({
-  primary500: '#181b1d',
-  primary600: '#303234',
-  primary700: '',
+  primary500: '#303234',
+  primary600: '#616264',
   secondary500: '#ffd60a',
   secondary600: '#ffc300',
-  secondary700: '',
   paleWhite: '#f0f1e9',
   fontBold: 'Poppins-Bold',
   fontRegular: 'Poppins-Regular',

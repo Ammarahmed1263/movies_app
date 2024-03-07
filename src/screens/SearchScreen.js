d5487189edbaea35f18f093d40fa7d7e77ba6246
Ammar Ahmed <ammarahmed1263@gmail.com>
2024-03-07 13:34:33 +0200

    add initial search bar and primary color shade

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,27 +1,12 @@
-import { TextInput, StyleSheet, View, Image } from "react-native";
+import { View } from "react-native";
+import SearchBar from "../components/ui/SearchBar";
 
 function SearchScreen() {
   return (
-    <View style={styles.searchBar}>
-      <TextInput 
-        placeholder="Search Movies"
-      />
-      <View style={{width: 50, height: 50, borderRadius: 50, backgroundColor: 'gray'}} ></View>
+    <View>
+      <SearchBar />      
     </View>
   )
 }
 
 export default SearchScreen;
-
-const styles = StyleSheet.create({
-  searchBar: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    borderWidth: 2,
-    borderColor: 'black',
-    marginTop: 20,
-    borderRadius: 50,
-    marginHorizontal: 15
-  }
-})
--- src/utils/GlobalStyles.js
+++ src/utils/GlobalStyles.js
@@ -3,6 +3,7 @@ import { StyleSheet } from "react-native";
 const GlobalStyles = StyleSheet.create({
   primary500: '#303234',
   primary600: '#616264',
+  primary700: '#c1bcbc',
   secondary500: '#ffbb00',
   secondary600: '#f5d108',
   paleWhite: '#f0f1e9',

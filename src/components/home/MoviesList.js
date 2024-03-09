aae3a347bebf029ffc989c203bd34fcfeb35ad1b
Ammar Ahmed <ammarahmed1263@gmail.com>
2024-03-09 13:29:36 +0200

    add initial movie list component and move search bar component

---
+++ src/components/home/MoviesList.js
@@ -0,0 +1,53 @@
+import {FlatList, Text, View, StyleSheet} from 'react-native';
+import Button from '../ui/Button';
+import GlobalStyles from '../../utils/GlobalStyles';
+import MovieCard from './MovieCard';
+
+const renderMovie = ({item: movie}) => {
+  return <MovieCard movie={movie}/>;
+};
+
+function MoviesList({movies, topic}) {
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.heading}>
+        <Text style={styles.topicTitle}>{topic}</Text>
+        <Button flat textStyle={styles.button}>See all</Button>
+      </View>
+      <FlatList
+        data={movies.slice(0, 10)}
+        renderItem={renderMovie}
+        keyExtractor={item => item.id}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+      />
+    </View>
+  );
+}
+
+export default MoviesList;
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 20
+  },
+  heading: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 5,
+    marginHorizontal: 14,
+  },
+  topicTitle: {
+    fontSize: 27,
+    fontFamily: GlobalStyles.fontBold,
+    color: GlobalStyles.paleWhite,
+  },
+  button: {
+    fontSize: 18,
+    textTransform: 'none',
+    color: GlobalStyles.secondary500,
+    fontFamily: GlobalStyles.fontLight
+  },
+});

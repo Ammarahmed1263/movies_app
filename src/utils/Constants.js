1e456d8e90e33b8fbdcf659a3794312efe1ad880
Ammar Ahmed <ammarahmed1263@gmail.com>
2024-03-09 13:25:25 +0200

    collect app constants in a separate file

---
+++ src/utils/Constants.js
@@ -0,0 +1,11 @@
+const ENDPOINT = {
+  image: 'https://image.tmdb.org/t/p/original',
+  movies: {
+    now_playing: 'https://api.themoviedb.org/3/movie/now_playing'
+  }
+}
+
+export const API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MTVjMDE0NjIxZGNlNGNhNGYzMzI2NWQ2MzA5ZmZhMiIsInN1YiI6IjY1MjY2MGZmZmQ2MzAwNWQ3YjI3MjViZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9c-Qit7_k7d4XANLn3OS2uBhB2VqpZKoGqMZUtgw3fc';
+
+
+export default ENDPOINT;

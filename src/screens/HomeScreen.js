2635098249d4add65f25e44e7d3364dca0c0f5d9
AmmarAhmed <ammarahmed1263@gmail.com>
2024-09-10 00:14:33 +0300

    refactor homescreen for concurrency

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,7 +3,7 @@ import {RefreshControl, Text, ScrollView, StatusBar, View} from 'react-native';
 import MoviesSection from '@organisms/MoviesSection';
 import MoviesCarousel from '@organisms/MoviesCarousel';
 import {useTranslation} from 'react-i18next';
-import { useTheme } from '@contexts/ThemeContext';
+import {useTheme} from '@contexts/ThemeContext';
 import {
   getNowPlaying,
   getPopular,
@@ -19,37 +19,35 @@ function HomeScreen() {
   const [top_rated, settop_rated] = useState([]);
   const [upcoming, setupcoming] = useState([]);
   const [refreshing, setRefreshing] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const {t} = useTranslation();
-  const { theme, colors} = useTheme();
-  // status bar in rest of screens solution
-  // useFocusEffect(
-  //   useCallback(() => {
-  //     StatusBar.setTranslucent(true);
-  //     StatusBar.setBackgroundColor('transparent');
-
-  //     return () => {
-  //       console.log('ran here')
-  //       StatusBar.setTranslucent(false);
-  //       StatusBar.setBackgroundColor(colors.primary500);
-  //     };
-  //   }, [])
-  // );
+  const {theme, colors} = useTheme();
 
   useEffect(() => {
+    console.log('fetching started');
+    setIsLoading(true);
     (async () => {
       try {
-        const response = await getNowPlaying();
-        const response2 = await getPopular();
-        const response3 = await getTopRated();
-        const response4 = await getUpcoming();
-        setnow_playing(response.results);
-        setpopular(response2.results);
-        settop_rated(response3.results);
-        setupcoming(response4.results);
+        const [
+          nowPlayingResponse,
+          popularResponse,
+          topRatedResponse,
+          upcomingResponse,
+        ] = await Promise.allSettled([
+          getNowPlaying(),
+          getPopular(),
+          getTopRated(),
+          getUpcoming(),
+        ]);
+        setnow_playing(nowPlayingResponse.value.results)
+        setpopular(popularResponse.value.results)
+        settop_rated(topRatedResponse.value.results)
+        setupcoming(upcomingResponse.value.results)
       } catch (e) {
         console.log('Full error object:', e);
       } finally {
         console.log('fetching APIs data done');
+        setIsLoading(false);
       }
     })();
   }, []);
@@ -58,15 +56,10 @@ function HomeScreen() {
     setRefreshing(true);
     setTimeout(() => {
       setRefreshing(false);
-    }, 2000);
+    }, 5000);
   }, []);
 
-  if (
-    now_playing.length === 0 ||
-    popular.length === 0 ||
-    top_rated.length === 0 ||
-    upcoming.length === 0
-  ) {
+  if (isLoading) {
     return (
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
         <Text>loading...</Text>
@@ -88,20 +81,19 @@ function HomeScreen() {
             progressViewOffset={StatusBar.currentHeight}
             refreshing={refreshing}
             onRefresh={onRefresh}
-            colors={['red', 'blue', 'chocolate', 'white']}
+            colors={[colors.secondaryShadow, colors.secondary600]}
             tintColor={colors.primary500}
             progressBackgroundColor={colors.primary500}
           />
         }>
-      {/* > */}
-        <View style={{flex: 2}}>
+        <View style={{flex: 1.5}}>
           <MoviesCarousel movies={now_playing?.slice(0, 8)} />
         </View>
         <View style={{flex: 1}}>
           <MoviesSection movies={now_playing} topic={t('now playing')} seeAll />
+          <MoviesSection movies={popular} topic={t('popular')} seeAll />
           <MoviesSection movies={top_rated} topic={t('top rated')} seeAll />
           <MoviesSection movies={upcoming} topic={t('upcoming')} seeAll />
-          <MoviesSection movies={popular} topic={t('popular')} seeAll />
         </View>
       </ScrollView>
     </>

a47e50f228b9feee0bae9c979547d49b931a9a41
AmmarAhmed <ammarahmed1263@gmail.com>
2024-09-21 16:57:27 +0300

    create app text component

---
+++ src/components/atoms/AppText.tsx
@@ -0,0 +1,40 @@
+import {useTheme} from '@contexts/ThemeContext';
+import {FC, ReactNode} from 'react';
+import {Platform, Text, TextProps, TextStyle} from 'react-native';
+import { FontVariants } from 'types/themeTypes';
+
+interface AppTextProps extends TextProps {
+  variant?: FontVariants;
+  style?: TextStyle | TextStyle[];
+  children?: ReactNode;
+}
+const AppText: FC<AppTextProps> = ({
+  children,
+  style,
+  variant = 'regular',
+  ...props
+}) => {
+  const { colors, fonts} = useTheme();
+  const fontStyles = fonts[variant];
+  
+  return (
+    <Text
+      style={[
+        {
+          color: colors.paleShade,
+          fontFamily: fontStyles.fontFamily,
+          fontSize: fontStyles.fontSize,
+          fontWeight:
+            Platform.OS === 'android'
+              ? 'normal'
+              : (fontStyles.fontWeight as TextStyle['fontWeight']),
+        },
+        style,
+      ]}
+      {...props}>
+      {children}
+    </Text>
+  );
+};
+
+export default AppText;

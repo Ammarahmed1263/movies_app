1e08e4bc910929e47208acc179bc11a6463bec19
AmmarAhmed <ammarahmed1263@gmail.com>
2024-09-21 17:15:32 +0300

    refactor movie carousel

--- src/components/organisms/MoviesCarousel.tsx
+++ src/components/organisms/MoviesCarousel.tsx
@@ -1,30 +1,20 @@
-import {FC, useState} from 'react';
-import {
-  ImageBackground,
-  View,
-  Text,
-  Dimensions,
-  StyleSheet,
-  StatusBar,
-  I18nManager,
-} from 'react-native';
+import {FC, useRef, useState} from 'react';
+import {ImageBackground, View, StyleSheet, StatusBar} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
-// import Carousel, {
-//   Pagination,
-// } from 'react-native-snap-carousel';
 import Carousel, {ICarouselInstance} from 'react-native-reanimated-carousel';
 import Pagination from '@molecules/Pagination';
-import CarouselItem from '@molecules/CarouselItem';
 import {useTheme} from '@contexts/ThemeContext';
 import {getImageUrl} from '@utils/index';
 import {Movie, MovieArray} from 'types/movieTypes';
-import { vs } from '@styles/metrics';
-
-const {width} = Dimensions.get('window');
-
-const renderItem = ({item, index}: {item: Movie; index: number}) => {
-  return <CarouselItem item={item} key={index + item.id}/>;
-};
+import {hs, vs, width} from '@styles/metrics';
+import AppText from '@atoms/AppText';
+import MovieCard from '@molecules/MovieCard';
+import Animated, {
+  Extrapolation,
+  interpolate,
+  SharedValue,
+  useAnimatedStyle,
+} from 'react-native-reanimated';
 
 interface MoviesCarouselProps {
   movies: MovieArray;
@@ -32,17 +22,62 @@ interface MoviesCarouselProps {
 
 const MoviesCarousel: FC<MoviesCarouselProps> = ({movies}) => {
   const [activeMovieIndex, setActiveMovieIndex] = useState<number>(0);
-  const {colors, fonts} = useTheme();
+  const carouselRef = useRef<ICarouselInstance>(null);
+  const {colors} = useTheme();
 
   const handleSnapToItem = (index: number) => {
     setActiveMovieIndex(index);
   };
 
-  // modify with better one
+  const handleDotPress = (index: number) => {
+    setActiveMovieIndex(index);
+    if (carouselRef.current) {
+      carouselRef?.current?.scrollTo({index});
+    }
+  };
+
+  const renderItem = ({
+    item,
+    index,
+    animationValue,
+  }: {
+    item: Movie;
+    index: number;
+    animationValue: SharedValue<number>;
+  }) => {
+    const animatedStyle = useAnimatedStyle(() => {
+      const opacity = interpolate(
+        animationValue.value,
+        [-1, 0, 1],
+        [0.4, 1, 0.4],
+        Extrapolation.CLAMP,
+      );
+
+      return {
+        opacity,
+      };
+    });
+
+    return (
+      <Animated.View style={[styles.carouselItem, animatedStyle]}>
+        <MovieCard
+          titleVariant="subheading"
+          movie={item}
+          key={index + item.id}
+          style={styles.carouselItem}
+          ImageViewStyle={{
+            height: '87%',
+          }}
+          hideVote
+        />
+      </Animated.View>
+    );
+  };
+
   if (!movies || movies.length === 0) {
     return (
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Text>Loading...</Text>
+        <AppText>Loading...</AppText>
       </View>
     );
   }
@@ -56,62 +91,57 @@ const MoviesCarousel: FC<MoviesCarouselProps> = ({movies}) => {
         colors={['transparent', colors.primary500]}
         style={styles.gradientContainer}>
         <View style={styles.headerContainer}>
-          <Text
-            style={{
-              ...styles.headerText,
-              fontFamily: fonts.bold,
-              color: colors.paleShade,
-            }}>
-            <Text>Movie</Text>
-            <Text style={{color: colors.secondary500, fontSize: 29}}>Corn</Text>
-          </Text>
+          <View style={styles.appName}>
+            <AppText
+              variant="heading"
+              style={{
+                fontSize: 35,
+                color: colors.paleShade,
+              }}>
+              Movie
+            </AppText>
+            <AppText
+              variant="heading"
+              style={{color: colors.secondary500, fontSize: 25}}>
+              Corn
+            </AppText>
+          </View>
         </View>
-        {/* <Carousel
-          data={movies}
-          renderItem={renderItem}
-          itemWidth={width - 175}
-          sliderWidth={width}
-          onBeforeSnapToItem={handleSnapToItem}
-          // inactiveSlideScale={0.86}
-          // inverted={I18nManager.isRTL}
-          // autoplay
-          inactiveSlideOpacity={0.5}
-          inactiveSlideScale={0.85}
-          loop
-        /> */}
+
         <Carousel
+          ref={carouselRef}
           data={movies}
           renderItem={renderItem}
           width={width}
+          defaultIndex={activeMovieIndex}
           style={{
             flex: 1,
-            width: width,
-            height: width * 1.1,
-            alignItems: 'center',
-            justifyContent: 'center',
+            height: vs(390),
           }}
           mode="parallax"
           modeConfig={{
             parallaxScrollingScale: 1,
-            parallaxScrollingOffset: 170,
-            parallaxAdjacentItemScale: 0.85,
+            parallaxScrollingOffset: width / 2 - hs(5),
+            parallaxAdjacentItemScale: 0.87,
           }}
           onSnapToItem={handleSnapToItem}
           pagingEnabled
         />
+
         <Pagination
           containerStyle={{marginTop: -10}}
           dotsLength={movies.length}
           activeDotIndex={activeMovieIndex}
           dotStyle={{
-            width: 10,
-            height: 10,
-            borderRadius: 5,
+            width: 15,
+            height: 15,
+            borderRadius: 8,
             backgroundColor: colors.secondary500,
           }}
           inactiveDotStyle={{backgroundColor: colors.primary700}}
           inactiveDotOpacity={0.6}
           inactiveDotScale={0.4}
+          setActiveIndex={handleDotPress}
         />
       </LinearGradient>
     </ImageBackground>
@@ -123,7 +153,7 @@ export default MoviesCarousel;
 const styles = StyleSheet.create({
   backgroundImage: {
     flex: 1,
-    minHeight: vs(400)
+    minHeight: vs(400),
   },
   gradientContainer: {
     marginTop: StatusBar?.currentHeight ?? 50,
@@ -132,7 +162,15 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'center',
   },
-  headerText: {
-    fontSize: 35,
+  appName: {
+    flexDirection: 'row',
+    alignItems: 'baseline',
+  },
+  carouselItem: {
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    paddingTop: 10,
+    aspectRatio: 8.5 / 16,
+    alignSelf: 'center',
   },
 });

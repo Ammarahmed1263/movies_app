9c7d582e423a398be3a90835ef2196bcc57e835f
AmmarAhmed <ammarahmed1263@gmail.com>
2024-09-27 19:51:59 +0300

    use network status

--- App.tsx
+++ App.tsx
@@ -1,16 +1,23 @@
 import ThemeProvider from './src/contexts/ThemeContext';
 import AppNavigation from './src/navigation/AppNavigation';
 import {I18nManager} from 'react-native';
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import i18n from './src/i18n';
+import useNetworkStatus from '@hooks/useNetworkStatus';
 
 export default function App() {
+  const isNetworkConnected = useNetworkStatus();
+
   useEffect(() => {
     if (i18n.language === 'ar') {
       I18nManager.forceRTL(true);
     } else {
       I18nManager.forceRTL(false);
+    }
+
+    if (!isNetworkConnected) {
+      console.warn("device isn't connected to the internet");
     }
   }, []);
 
---
+++ src/hooks/useNetworkStatus.tsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+import NetInfo from '@react-native-community/netinfo';
+
+const useNetworkStatus = () => {
+  const [isConnected, setIsConnected] = useState<boolean | null>(true);
+
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener((state) => {
+      setIsConnected(state.isConnected);
+    });
+
+    return () => {
+      unsubscribe(); // Clean up the subscription on unmount
+    };
+  }, []);
+
+  return isConnected;
+};
+
+export default useNetworkStatus;

49d2b6ea42fff1772f0bb2418050bcd06b036e97
Ammarahmed1263 <ammarahmed1263@gmail.com>
2025-01-25 22:01:06 +0200

    new search screen

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,107 +1,103 @@
-import {Platform, SafeAreaView, StatusBar, TextInput, View} from 'react-native';
-import MoviesList from '../components/organisms/MoviesList';
-import SearchBar from '@molecules/SearchBar';
-import {useCallback, useEffect, useReducer, useState} from 'react';
-import {searchMovies} from '@services/movieService';
-import {SearchResult} from 'types/searchTypes';
-import LottieView from 'lottie-react-native';
-import {hs, vs, width} from '@styles/metrics';
 import AppLoading from '@atoms/AppLoading';
 import AppText from '@atoms/AppText';
-import { useMovieSearch } from '@hooks/useMovieSearch';
-
+import useDebouncedSearch from '@hooks/useDebouncedSearch';
+import EmptySearch from '@molecules/EmptySearch';
+import SearchBar from '@molecules/SearchBar';
+import {hs, vs} from '@styles/metrics';
+import {useState} from 'react';
+import {
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  View,
+} from 'react-native';
+import {MovieSummary} from 'types/movieTypes';
+import MoviesList from '../components/organisms/MoviesList';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 function SearchScreen() {
-  const [keyword, setkeyword] = useState('');
-  const { searchResults, page, total_pages, loading , handleSearch, dispatch } = useMovieSearch();
+  const [keyword, setKeyword] = useState('');
+  const insets = useSafeAreaInsets();
+  const {results, status, loadMore, hasMore, page} =
+    useDebouncedSearch(keyword);
 
+  const showLoading = (size: number, speed: number) => {
+    return (
+      <AppLoading
+        source={require('../assets/lottie/loading_fade.json')}
+        size={size}
+        speed={speed}
+      />
+    );
+  };
 
-  useEffect(() => {
-    if (keyword === '') {
-      dispatch({type: 'RESET_SEARCH'});
-      dispatch({type: 'STOP_LOADING'});
-      return;
+  const renderContent = () => {
+    if (!keyword) {
+      return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <AppText variant="heading">Search for movies</AppText>
+        </View>
+      );
     }
 
-    dispatch({type: 'SET_LOADING'});
-    const id = setTimeout(() => handleSearch(keyword, page), 500);
-    // dispatch({type: 'STOP_LOADING'});
-    return () => clearTimeout(id);
-  }, [keyword]);
-
-  const handlePagination = async () => {
-    if (page < total_pages && !loading) {
-      dispatch({type: 'SET_LOADING'});
-      const response = await searchMovies({
-        query: keyword,
-        page: page + 1,
-      });
-      dispatch({type: 'SET_RESULTS', payload: response});
+    if (status === 'searching') {
+      return showLoading(70, 1.8);
     }
+
+    if (status === 'empty') {
+      return <EmptySearch keyword={keyword} />;
+    }
+
+    return (
+      <MoviesList
+        data={results as MovieSummary[]}
+        onEndReached={loadMore}
+        onEndReachedThreshold={0.7}
+        columnWrapperStyle={styles.columnWrapper}
+        contentContainerStyle={[
+          styles.contentContainer,
+          {
+            paddingBottom:
+              Platform.OS === 'android'
+                ? vs(45) + insets.bottom + vs(85)
+                : vs(85),
+          },
+        ]}
+        numColumns={2}
+        snapStyle={{bottom: vs(100)}}
+        ListFooterComponent={
+          status === 'paginating' ? showLoading(35, 2.5) : null
+        }
+      />
+    );
   };
 
   return (
-    <SafeAreaView style={{flex: 1, marginTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight}}>
-      <SearchBar keyword={keyword} setKeyword={setkeyword} />
-      {loading && searchResults.length === 0 ? (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-          <AppLoading source={require('../assets/lottie/loading_fade.json')} size={70} speed={1.8}/>
-        </View>
-      ) : (
-        <View style={{flex: 1}}>
-          {keyword ? (
-            <MoviesList
-              data={searchResults}
-              onEndReached={handlePagination}
-              columnWrapperStyle={{justifyContent: 'flex-start', gap: hs(10), marginVertical: vs(12)}}
-              contentContainerStyle={{flexGrow: 1, paddingBottom: vs(80), marginHorizontal: hs(10)}}
-              numColumns={2}
-              snapStyle={{bottom: vs(100)}}
-              ListEmptyComponent={
-                keyword && searchResults.length === 0 ? (
-                  <View
-                    style={{
-                      flex: 1,
-                      alignItems: 'center',
-                      justifyContent: 'center',
-                      paddingHorizontal: 30,
-                      marginBottom: 100
-                    }}>
-                    <LottieView
-                      source={Platform.OS === 'ios' ? require('../assets/lottie/no_search_results(2).json') : require('../assets/lottie/no_search_results.json')}
-                      style={{width: Platform.OS === 'ios' ? width * 0.7 : width * 0.8, aspectRatio: 1 / 1}}
-                      autoPlay
-                      loop
-                    />
-                    <AppText variant="heading" style={{textAlign: 'center'}}>
-                      Ooops...No movie found with {keyword}!
-                    </AppText>
-                  </View>
-                ) : null
-              }
-              ListFooterComponent={
-                page < total_pages &&
-                searchResults.length !== 0 ? (
-                  <View style={{alignItems: 'center', paddingBottom: vs(20)}}>
-                    <AppLoading
-                      size={35}
-                      speed={2.5}
-                      source={require('../assets/lottie/loading_fade.json')}
-                    />
-                  </View>
-                ) : null
-              }
-            />
-          ) : (
-            <View
-              style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-              <AppText variant="heading">search here</AppText>
-            </View>
-          )}
-        </View>
-      )}
+    <SafeAreaView style={styles.container}>
+      <SearchBar keyword={keyword} setKeyword={setKeyword} />
+      {renderContent()}
     </SafeAreaView>
   );
 }
 
 export default SearchScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight,
+  },
+  loading: {
+    backgroundColor: 'red',
+  },
+  columnWrapper: {
+    justifyContent: 'flex-start',
+    gap: hs(10),
+    marginVertical: vs(12),
+  },
+  contentContainer: {
+    flexGrow: 1,
+    marginHorizontal: hs(10),
+  },
+});

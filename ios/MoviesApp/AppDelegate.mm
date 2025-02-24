26e9e772129088caef193a01f99934e72a8ec729
Ammar Ahmed <ammarahmed1263@gmail.com>
2025-02-25 00:14:03 +0200

    ios splash screen

--- ios/MoviesApp/AppDelegate.mm
+++ ios/MoviesApp/AppDelegate.mm
@@ -1,19 +1,22 @@
 #import "AppDelegate.h"
 #import <Firebase.h>
 #import <React/RCTBundleURLProvider.h>
+#import "RNSplashScreen.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   [FIRApp configure];
-  
   self.moduleName = @"MoviesApp";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
 
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+  [super application:application didFinishLaunchingWithOptions:launchOptions];
+  [RNSplashScreen show];
+
+  return YES;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
